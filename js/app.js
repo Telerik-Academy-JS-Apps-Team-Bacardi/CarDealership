7c535001f6a0182df2cb5fbdc363bf03a932b7df
Nikolai Novkirishki <novkirishki@yahoo.com>
2015-08-27 16:28:37 +0300

    Removed angular and added sammy.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 'use strict';
 
-var app = angular
-    .module('carDealApp', ['ngResource', 'ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider
-            .when('/home', {
-                templateUrl: 'views/home.html'
-            })
-            .otherwise({redirectTo: '/home'});
-    })
+var app = Sammy('#viewContainer', function (){
+	
+	this.get('#/', function() {
+		this.partial('views/enterApp.html');
+	});
+	
+	this.get('#/home', function() {
+		this.partial('views/home.html');
+	});
+	
+	this.get('#/offer/:id', function() {
+		alert('Watching offer details for offer with id: ' + this.params['id']);
+	});
+});
+app.run('#/');
---
+++ js/controllers/addOfferCtrl.js
@@ -0,0 +1,2 @@
+Parse.initialize("BxC62zFfCXJAfLxS90r6hwNSz0OIKtDlZ1sVeCCV", "Av5f9x57L6qsWpxohLSaXtqUD32Pblzm4dyUnYaJ");
+
