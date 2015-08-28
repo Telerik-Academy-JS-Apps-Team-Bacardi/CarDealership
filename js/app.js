896b28df1cfa4bcb12ed2b3ae568f57ac377e308
Nikolai Novkirishki <novkirishki@yahoo.com>
2015-08-28 10:13:37 +0300

    Added new offer view

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ var app = Sammy('#viewContainer', function (){
 		this.partial('views/home.html');
 	});
 	
+	this.get('#/newOffer', function() {
+		this.partial('views/newOffer.html');
+	});
+	
 	this.get('#/offer/:id', function() {
 		alert('Watching offer details for offer with id: ' + this.params['id']);
 	});
