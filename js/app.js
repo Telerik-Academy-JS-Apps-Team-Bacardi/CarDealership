a771892a3e3b851cb29183120378d83e4c9f71f6
Nikolai Novkirishki <novkirishki@yahoo.com>
2015-08-28 11:28:40 +0300

    Added newOffer controller and connected the form with the database

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 'use strict';
 
-var app = Sammy('#viewContainer', function (){
-	
-	this.get('#/', function() {
+var app = Sammy('#viewContainer', function () {
+
+	this.get('#/', function () {
 		this.partial('views/enterApp.html');
 	});
-	
-	this.get('#/home', function() {
+
+	this.get('#/home', function () {
 		this.partial('views/home.html');
 	});
-	
-	this.get('#/newOffer', function() {
-		this.partial('views/newOffer.html');
+
+	this.get('#/newOffer', function () {
+		this.partial('views/newOffer.html')
+			.then(function() {
+				addOffer.add();
+			});
 	});
-	
-	this.get('#/offer/:id', function() {
+
+	this.get('#/offer/:id', function () {
 		alert('Watching offer details for offer with id: ' + this.params['id']);
 	});
 });
--- js/controllers/addOfferCtrl.js
+++ js/controllers/addOfferCtrl.js
@@ -1,2 +1,47 @@
-Parse.initialize("BxC62zFfCXJAfLxS90r6hwNSz0OIKtDlZ1sVeCCV", "Av5f9x57L6qsWpxohLSaXtqUD32Pblzm4dyUnYaJ");
+var addOffer = (function () {
+	return {
+		add: function () {
+			Parse.initialize("BxC62zFfCXJAfLxS90r6hwNSz0OIKtDlZ1sVeCCV", "Av5f9x57L6qsWpxohLSaXtqUD32Pblzm4dyUnYaJ");
 
+			var sendOfferButton = $('#submitOffer');
+			
+			sendOfferButton.on('click', function () {
+				var Offer = Parse.Object.extend('Offer'),
+					offer = new Offer(),
+					category = $('#category').val(),
+					manufacturer = $('#manufacturer').val(),
+					model = $('#model').val(),
+					year = $('#year').val(),
+					price = $('#price').val(),
+					description = $('#description').val(),
+					isNew = $('input[name="condition"]:checked').val();
+					
+				// convert isNew value to boolean	
+				if(isNew === 'true') {
+					isNew = true;
+				} else {
+					isNew = false;
+				}
+
+				offer.set('Category', category);
+				offer.set('Manufacturer', manufacturer);
+				offer.set('Model', model);
+				offer.set('Year', +year);
+				offer.set('Price', +price);
+				offer.set('isNew', isNew);
+				offer.set('Description', description);
+
+				offer.save()
+					.then(function () {
+						var query = new Parse.Query(Offer);
+						return query.find();
+					})
+					.then(function (offers) {
+						offers.forEach(function (offer) {
+							console.log(offer.get('Manufacturer') + ' ' + offer.get('Model'));
+						})
+					})
+			})
+		}
+	}
+} ());
