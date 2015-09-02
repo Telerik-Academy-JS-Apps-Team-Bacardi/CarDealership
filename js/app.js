b88431e366cd34060dd87e5bd58f614f6c03604e
Nikolai Novkirishki <novkirishki@yahoo.com>
2015-09-02 13:38:51 +0300

    Refactoring

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ var app = Sammy('#viewContainer', function () {
 	this.get('#/userLogin', function () {
 		this.partial('views/userLogin.html')
 			.then(function () {
-				addUser.add();
+				registerUserCtrl.render();
 			});
 	});
 	
--- js/controllers/addUserNavBar.js
+++ js/controllers/addUserNavBar.js
@@ -1,6 +1,5 @@
 
 var loginNavBar = (function () {
-
     Parse.initialize("BxC62zFfCXJAfLxS90r6hwNSz0OIKtDlZ1sVeCCV", "Av5f9x57L6qsWpxohLSaXtqUD32Pblzm4dyUnYaJ");
     
     var currentUser = Parse.User.current();
