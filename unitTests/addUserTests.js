5e9c2293dfbc8b48af661b65c5a7f4c6330c62dc
vvn050 <nikolov.vladislav@gmail.com>
2015-09-04 10:50:08 +0300

    Add users Unit tests

--- unitTests/addUserTests.js
+++ unitTests/addUserTests.js
@@ -5,25 +5,28 @@
 
     describe('Testing password validation', function () {
         it('expects to return false if passwords do not match', function () {
-            expect(addUser.add().validatePassword('pass', 'pass2')).to.be.false;
+            expect(validator.validatePassword('pass', 'pass2')).to.be.false;
         });
         it('expects to return false if password is too long', function () {
-            expect(addUser.add().validatePassword('3213213123132131232132131231321312', '3213213123132131232132131231321312')).to.be.false;
+            expect(validator.validatePassword('3213213123132131232132131231321312', '3213213123132131232132131231321312')).to.be.false;
         });
         it('expects to return false if password is too short /testing ints/', function () {
-            expect(addUser.add().validatePassword(2, 2)).to.be.false;
+            expect(validator.validatePassword(2, 2)).to.be.false;
         });
         it('expects to return false if password is too short /testing strings/', function () {
-            expect(addUser.add().validatePassword('2', '2')).to.be.false;
+            expect(validator.validatePassword('2', '2')).to.be.false;
         })
     });
 
     describe('Testing first name and last name /user data/', function () {
         it('expects to return false if first or last name are not a string', function () {
-            expect(addUser.add().validateUserNames(2, 'kolio')).to.be.false;
+            expect(validator.validateUserNames(2, 'kolio')).to.be.false;
         });
         it('expects to return false if first name or last name is missing', function () {
-            expect(addUser.add().validateUserNames('kolio')).to.be.false;
+            expect(validator.validateUserNames('kolio')).to.be.false;
+        });
+        it('expects to return true if given two strings', function () {
+            expect(validator.validateUserNames('kolcho', 'koliov')).to.be.true;
         });
     });
 
@@ -31,16 +34,16 @@
          Parse.initialize("BxC62zFfCXJAfLxS90r6hwNSz0OIKtDlZ1sVeCCV", "BxC62zFfCXJAfLxS90r6hwNSz0OIKtDlZ1sVeCCV");
 
         it('expects email to have @', function () {
-           expect(addUser.add().validateUserEmail('kolio')).to.be.false;
+           expect(validator.validateUserEmail('kolio')).to.be.false;
         });
         it('expects email to have symbols between @ and the domain', function () {
-           expect(addUser.add().validateUserEmail('kolio@.bg')).to.be.false;
+           expect(validator.validateUserEmail('kolio@.bg')).to.be.false;
         });
          it('expects email to have domain', function () {
-           expect(addUser.add().validateUserEmail('kolio@gmail')).to.be.false;
+           expect(validator.validateUserEmail('kolio@gmail')).to.be.false;
         });
          it ('expects email with valid domain and @ to pass', function (){
-             expect(addUser.add().validateUserEmail('kolio23@abv.bg')).to.be.true;
+             expect(validator.validateUserEmail('kolio23@abv.bg')).to.be.true;
          });
 
     });
