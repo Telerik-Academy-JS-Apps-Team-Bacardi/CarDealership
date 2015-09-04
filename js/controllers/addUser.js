aaaa2d3aa42f8a959b55c8ae5ab8642da74d94e4
vvn050 <nikolov.vladislav@gmail.com>
2015-09-04 11:06:03 +0300

    Logout not appearing fix

--- js/controllers/addUser.js
+++ js/controllers/addUser.js
@@ -56,6 +56,7 @@ var registerUserCtrl = (function () {
                         window.history.back();
                         
                     }).then(function () {
+                        loginNavBar();
                         window.location.reload(true);
                     });
                 }
